2891e1fd867cd2ea52d636466c987c143b67e851
James Hornung <hornung.james@gmail.com>
2016-07-25 19:45:33 -0500

    corrected connection

--- app/config/connection.js
+++ app/config/connection.js
@@ -26,7 +26,7 @@ var source = {
 
 
 // TODO: connect your jawsDB connection witht he mysql connection.
-var connection = mysql.createConnection(jawsDB);
+var connection = mysql.createConnection(source.jawsDB);
 
 
 
